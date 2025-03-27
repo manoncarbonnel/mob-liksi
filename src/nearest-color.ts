27fb77f69e4c81139ca4c8df30b5ade288d6d342
Manon Carbonnel <manon.carbonnel@gmail.com>
2025-03-27 14:35:50 +0100

    init kata

--- src/nearest-color.ts
+++ src/nearest-color.ts
@@ -1,1 +1,6 @@
-export const nearestColor = () => {};
+export const nearestColor = (color: string) => {
+  if (color === 'F42') {
+    return 'F00';
+  }
+  return color;
+};
--- tests/nearest-color.spec.ts
+++ tests/nearest-color.spec.ts
@@ -2,7 +2,18 @@ import { describe, it, expect } from 'vitest';
 import { nearestColor } from 'src/nearest-color';
 
 describe('', () => {
-  it('', () => {
-    expect(nearestColor()).toBe(undefined);
+  it('teste que la couleur la plus proche de F00 est F00', () => {
+    expect(nearestColor('F00')).toEqual('F00');
+  });
+  it('teste que la couleur la plus proche de 0F0 est 0F0', () => {
+    expect(nearestColor('0F0')).toEqual('0F0');
+  });
+
+  it('teste que la couleur la plus proche de F42 est F00', () => {
+    expect(nearestColor('F42')).toEqual('F00');
+  });
+
+  it('teste que la couleur la plus proche de 00F est 00F', () => {
+    expect(nearestColor('00F')).toEqual('00F');
   });
 });
